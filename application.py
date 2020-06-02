5695fc32c5ab00367835f6cec79c07ca381d1829
yogeshsingh <yogeshsingh101200@gmail.com>
2020-06-02 12:15:38 +0530

    Started git repo: added intial project files

---
+++ application.py
@@ -0,0 +1,16 @@
+""" Controller for flack web app """
+
+import os
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+socketio = SocketIO(app)
+
+
+@app.route("/")
+def index():
+    """ Todo """
+    return render_template("index.html")
