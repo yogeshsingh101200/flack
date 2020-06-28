683f2862a9d4713753e51d20d1f59ab5cbff861d
yogeshsingh <yogeshsingh101200@gmail.com>
2020-06-28 22:37:50 +0530

    fixed bug in random_hex_darkcolor()

--- helpers.py
+++ helpers.py
@@ -22,10 +22,10 @@ def signin_required(func):
     return decorated_function
 
 
-def dark(color):
+def dark(red, green, blue):
     """ Checks if a color is dark or not """
-    darkness = 1 - (0.299 * color[0] + 0.587 *
-                    color[1] + 0.114 * color[2]) / 255
+    darkness = 1 - (0.299 * red + 0.587 *
+                    green + 0.114 * blue) / 255
 
     if darkness > 0.5:
         return True
@@ -34,18 +34,19 @@ def dark(color):
 
 def random_hex_darkcolor():
     """ Return hexcode of a random dark color """
-
-    color = []
-
     itr = 0
-    while itr < 10000:
-        for _ in range(3):
-            random.seed(os.urandom(64))
-            color.append(random.randrange(0, 256))
+    rgb = [0, 0, 0]
 
-        if dark(color):
+    while itr < 10000:
+        for idx in range(3):
+            random.seed(os.urandom(64))
+            rgb[idx] = random.randint(0, 255)
+
+        red, green, blue = rgb
+
+        if dark(red, green, blue):
             break
 
         itr += 1
 
-    return "#{:02x}{:02x}{:02x}".format(color[0], color[1], color[2])
+    return "#{:02x}{:02x}{:02x}".format(red, green, blue)
