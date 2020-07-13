e535800f2528b6836ca582b630c19d8414b070d5
yogeshsingh <yogeshsingh101200@gmail.com>
2020-07-14 00:57:28 +0530

    removed location.port for to use socketio on heroku

--- static/index.js
+++ static/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     // Connect to websocket
-    var socket = io.connect(location.protocol + "//" + document.domain + ":" + location.port);
+    var socket = io.connect(location.protocol + "//" + document.domain);
 
     // By default, submit button is disabled
     document.querySelector("#btn-create-channel").disabled = true;
